0773f996ce905530a37cb53c8c2664b55bf76094
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2025-02-22 12:14:48 +0530

    init: clerk and shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,185 @@
+import Footer from "@/components/Footer";
+import { Navbar } from "@/components/Navbar";
+import BlurFade from "@/components/magicui/blur-fade";
+import Perks from "@/components/perks";
+import TypewriterEffectSmoothDemo from "@/components/typewritter";
+import RotatingText from "@/components/ui/RotatingText";
+import { Button } from "@/components/ui/button";
+import { LampContainer } from "@/components/ui/lamp";
+import SparklesText from "@/components/ui/sparklestxt";
+import { TextHoverEffect } from "@/components/ui/text-hover-effect";
+import { SignUpButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import {
+  ArrowRightIcon,
+  InstagramIcon,
+  LinkedinIcon,
+  RocketIcon,
+  TwitterIcon,
+} from "lucide-react";
 import Link from "next/link";
 
-export default function HomePage() {
+export default function Home() {
+  const { userId } = auth();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
+    <div className="min-h-screen w-full overflow-hidden bg-black pt-20">
+      <Navbar />
+
+      <main className="container relative mx-auto px-4 sm:px-6 lg:px-8">
+        {/* Decorative elements */}
+
+        {/* Hero Section */}
+        <div className="relative py-20 text-center lg:py-32">
+          <RocketIcon className="mx-auto mb-6 h-16 w-16 animate-bounce text-purple-500" />
+          <h1 className="mb-6 bg-gradient-to-r from-pink-600 via-violet-500 to-cyan-300 bg-clip-text text-4xl font-extrabold text-transparent sm:text-5xl lg:text-6xl">
+            Sky-Rocket Your Carrier with{" "}
+            <span className="text-8xl">
+              <SparklesText text="CazzAI" />{" "}
+            </span>
+          </h1>
+
+          <div className="flex justify-center space-x-4">
+            <Button
+              asChild
+              className="transform rounded-full bg-blue-600 px-8 py-3 text-lg text-white transition duration-300 ease-in-out hover:scale-105 hover:bg-blue-700"
+            >
+              <Link href="/generate">Start Creating</Link>
+            </Button>
+            <Button
+              asChild
+              className="rounded-full border border-blue-600 bg-transparent from-pink-700 via-purple-500 to-cyan-300 px-8 py-3 text-lg text-blue-600 transition duration-300 ease-in-out hover:bg-gradient-to-r hover:text-white"
+            >
+              <Link href="#features">Learn More</Link>
+            </Button>
+          </div>
         </div>
-      </div>
-    </main>
+
+        {/* Features Section */}
+        <div className=" " id="features">
+          <h1 className="h1 text-center">
+            <BlurFade delay={0.1} blur="6px" duration={1} inView>
+              <span className="">
+                <span className="inline-bloc mb-12 bg-gradient-to-r from-violet-500 to-fuchsia-500 bg-clip-text text-center text-4xl font-bold text-transparent">
+                  Services We Provide In{" "}
+                  <span>
+                    <SparklesText text="CazzAI" />{" "}
+                  </span>
+                </span>
+              </span>
+            </BlurFade>
+          </h1>
+          <div className="align-column-center py-10 text-center">
+            <BlurFade delay={0.1} blur="6px" duration={1} inView>
+              <RotatingText />
+            </BlurFade>
+          </div>
+
+          <div className="mx-auto grid max-w-5xl grid-cols-1 gap-10 py-20 md:grid-cols-3">
+            {[
+              {
+                title: "Twitter Threads",
+                icon: <TwitterIcon className="mb-4 h-10 w-10 text-blue-400" />,
+                description:
+                  "Generate compelling Twitter threads that engage your audience and boost your reach.",
+              },
+              {
+                title: "Instagram Captions",
+                icon: (
+                  <InstagramIcon className="mb-4 h-10 w-10 text-pink-400" />
+                ),
+                description:
+                  "Create catchy captions for your Instagram posts that increase engagement and followers.",
+              },
+              {
+                title: "LinkedIn Posts",
+                icon: <LinkedinIcon className="mb-4 h-10 w-10 text-blue-600" />,
+                description:
+                  "Craft professional content for your LinkedIn network to establish thought leadership.",
+              },
+            ].map((feature, index) => (
+              <div
+                key={index}
+                className="transform rounded-2xl bg-gradient-to-br from-gray-800 to-gray-900 p-8 shadow-lg transition duration-300 ease-in-out hover:-translate-y-1 hover:shadow-xl"
+              >
+                <div className="flex flex-col items-center text-center">
+                  {feature.icon}
+                  <h3 className="mb-3 text-2xl font-semibold text-white">
+                    {feature.title}
+                  </h3>
+                  <p className="text-gray-300">{feature.description}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Benefits Section */}
+        <Perks />
+
+        {/* CTA Section */}
+        <div className="relative py-20 text-center">
+          <div className="animate-spin-slow absolute right-10 top-10">
+            <svg
+              className="h-20 w-20 text-blue-500 opacity-20"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M12 6V12L16 14"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+          <h2 className="mb-8 bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 bg-clip-text text-4xl font-bold text-transparent">
+            Want to elevate yourself to one step ahead?
+          </h2>
+          {userId ? (
+            <Button
+              asChild
+              className="transform rounded-full bg-blue-600 from-pink-500 via-purple-500 to-indigo-500 px-10 py-4 text-lg text-white transition duration-300 ease-in-out hover:scale-105 hover:bg-gradient-to-r"
+            >
+              <Link href="/generate">
+                Generate Content Now <ArrowRightIcon className="ml-2 h-5 w-5" />
+              </Link>
+            </Button>
+          ) : (
+            <SignUpButton mode="modal">
+              <Button className="transform rounded-full bg-blue-600 px-10 py-4 text-lg text-white transition duration-300 ease-in-out hover:scale-105 hover:bg-blue-700">
+                Get Started Free <ArrowRightIcon className="ml-2 h-5 w-5" />
+              </Button>
+            </SignUpButton>
+          )}
+        </div>
+        <div className="relative mb--20 text-center text-2xl"></div>
+
+        <div>
+          <LampContainer>
+            <div className="relative flex w-full flex-col items-center justify-center text-center">
+              <TypewriterEffectSmoothDemo />
+            </div>
+          </LampContainer>
+        </div>
+
+        <div>
+          <Footer />
+        </div>
+
+        <div className="hidden h-[20rem] items-center justify-center md:flex lg:h-[20rem]">
+          <TextHoverEffect text="CAZZAI" />
+        </div>
+      </main>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,126 @@
-import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
-
-export default {
-  content: ["./src/**/*.tsx"],
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { Config } from "tailwindcss";
+import svgToDataUri from "mini-svg-data-uri";
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        glow: {
+          "0%, 100%": { boxShadow: "0 0 10px rgba(150, 0, 255, 0.5)" },
+          "50%": { boxShadow: "0 0 20px rgba(150, 0, 255, 1)" },
+        },
+      },
+      animation: {
+        "glow-purple": "glow 2s infinite",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "background-shine": "background-shine 2s linear infinite",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#ffffff",
+        black: "#000000",
+        lightblue: "#DAEBFF",
+        lightgrey: "#AEC7E4",
+        navyblue: "#002834",
+        beach: "#8EA9C1",
+        circlebg: "rgba(77, 213, 143, 0.25)",
+        darkblue: " #000321",
+        offwhite: "rgba(255, 255, 255, 0.75);",
+        bordertop: "rgba(196, 196, 196, 0.5);",
+        darkgray: "#90A3B4",
+        babyblue: "#E2F3F9",
+        grey500: "#ECECEC",
+        bluegray: "#7D82A1",
+        bluegrey: "#7C8F9E",
+        midnightblue: "#183B56",
+        midblue: "#00276F",
+        bluebg: "rgba(47, 184, 227, 0.2)",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [
+    require("daisyui"),
+    require("tailwindcss-animate"),
+    require("@tailwindcss/aspect-ratio"),
+    addVariablesForColors,
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          "bg-dot-thick": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`,
+            )}")`,
+          }),
+        },
+        {
+          values: flattenColorPalette(theme("backgroundColor")),
+          type: "color",
+        },
+      );
+    },
+  ],
+};
+export default config;
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  const allColors = flattenColorPalette(theme("colors"));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
